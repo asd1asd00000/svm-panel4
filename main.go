021170c3a45885e5d148f5370e6325a296586e57
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2026-07-18 18:57:04 +0330

    Update import paths to svm-panel4

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/asd1asd00000/svm-panel/api"
-	"github.com/asd1asd00000/svm-panel/database"
-	"github.com/asd1asd00000/svm-panel/sshvpn"
+	"github.com/asd1asd00000/svm-panel4/api"
+	"github.com/asd1asd00000/svm-panel4/database"
+	"github.com/asd1asd00000/svm-panel4/sshvpn"
 )
 
 const (
