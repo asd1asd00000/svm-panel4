073e5e60b2556f047ca437234ccf578897298a12
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2026-07-18 18:54:22 +0330

    Update agent.go

--- api/agent.go
+++ api/agent.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/asd1asd00000/svm-panel/database"
-	"github.com/asd1asd00000/svm-panel/sshvpn"
+	"github.com/asd1asd00000/svm-panel4/database"
+	"github.com/asd1asd00000/svm-panel4/sshvpn"
 )
 
 // registerAgentRoutes مسیرهایی که نودها/کلاینت‌های سرور اصلی برای همگام‌سازی کاربران و ترافیک استفاده می‌کنند را ثبت می‌کند
